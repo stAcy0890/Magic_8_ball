dc0c40befc4e11755811165e56c4f3bb6a878e20
stAcy0890 <stacysarfo@hotmail.com>
2021-01-08 15:35:24 +0000

    Initial commit...UI completed

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -12,7 +12,11 @@ class ViewController: UIViewController {
     
     let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
 
-
-
+    @IBOutlet weak var ballImage: UIImageView!
+    
+    @IBAction func askButton(_ sender: UIButton) {
+        ballImage.image = ballArray[2]
+    }
+    
 }
 
